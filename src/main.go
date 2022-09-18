7d32a096ee7d73800891213871da546ec27ededa
ofadiman <ofadiman@gmail.com>
2022-09-18 19:55:46 +0200

    feat: add swagger

--- src/main.go
+++ src/main.go
@@ -5,12 +5,32 @@ import (
 	"github.com/joho/godotenv"
 	"github.com/ofadiman/go-server/src/companies"
 	"github.com/ofadiman/go-server/src/database"
+	docs "github.com/ofadiman/go-server/src/docs"
 	"github.com/ofadiman/go-server/src/posts"
 	"github.com/ofadiman/go-server/src/profiles"
 	"github.com/ofadiman/go-server/src/users"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 	"log"
 	"net/http"
 )
+
+type PingResponseBody struct {
+	Message string `json:"message"`
+}
+
+// PingExample godoc
+// @Summary ping example
+// @Schemes
+// @Description ping server
+// @Tags ping
+// @Accept json
+// @Produce json
+// @success 200 {object} PingResponseBody
+// @Router /ping [get]
+func Ping(context *gin.Context) {
+	context.JSON(http.StatusOK, PingResponseBody{Message: "pong"})
+}
 
 func main() {
 	err := godotenv.Load()
@@ -26,11 +46,10 @@ func main() {
 
 	r := gin.Default()
 
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
+	docs.SwaggerInfo.BasePath = "/api/v1"
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
+
+	r.GET("/ping", Ping)
 
 	r.POST("/users", users.CreateUser)
 	r.GET("/users/:userId", users.GetUserById)
